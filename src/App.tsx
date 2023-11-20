82e4884d306126098caf1af52c89791b87f2f9c5
Dominik Stumpf <s.dominik.work@gmail.com>
2023-11-21 00:42:30 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+export default function App() {
+  return (
+    <div className="text-emerald-500">
+      test main
+    </div>
+  )
+}
