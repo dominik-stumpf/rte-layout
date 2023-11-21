f4f93283e03ceb58a93167588c111b2d7247c284
Dominik Stumpf <s.dominik.work@gmail.com>
2023-11-21 01:08:56 +0100

    base setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import { ThemeProvider } from '@/components/theme-provider';
 
 export default function App() {
   return (
-    <div className="text-emerald-500">
-      test main
-    </div>
-  )
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="w-screen min-h-screen grid place-items-center p-4">
+        <main className="border max-w-[85ch] w-full h-full max-h-[32rem] flex flex-col">
+          <Toolbar/>
+          <Editor/>
+          <Statusline/>
+        </main>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export function Editor() {
+  return <div>editor</div>;
+}
+
+export function Toolbar() {
+  return <div>toolbar</div>;
+}
+
+export function Statusline() {
+  return <div>toolbar</div>;
 }
