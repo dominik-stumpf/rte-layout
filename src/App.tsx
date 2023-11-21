8d62deaec813e37c92c137f6b1c86dbfb275a9ea
Dominik Stumpf <s.dominik.work@gmail.com>
2023-11-21 01:44:25 +0100

    main component layout css

--- src/App.tsx
+++ src/App.tsx
@@ -14,14 +14,26 @@ export default function App() {
   );
 }
 
-export function Editor() {
-  return <div>editor</div>;
+export function Toolbar() {
+  return <div className="border border-b-2 px-4">toolbar</div>;
 }
 
-export function Toolbar() {
-  return <div>toolbar</div>;
+export function Editor() {
+  return (
+    <div className="grow p-4 prose lg:prose-lg dark:prose-invert">
+      <h2>your regular text editor</h2>
+      <p>with a ready-to-go typographic setting :)</p>
+    </div>
+  );
 }
 
 export function Statusline() {
-  return <div>toolbar</div>;
+  const words = 14;
+  const characters = 238;
+
+  return (
+    <div className="border border-t-2 text-right text-muted bg-muted-foreground px-4">
+      Words: {words} Characters: {characters}
+    </div>
+  );
 }
